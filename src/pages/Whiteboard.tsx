d09500aaf7905b63f58ccf10634fb4fd35bec24c
Davis Chang <davischang1030@gmail.com>
2025-01-06 23:20:01 +0800

    feat: add excalidraw api

--- src/pages/Whiteboard.tsx
+++ src/pages/Whiteboard.tsx
@@ -1,11 +1,44 @@
-import { Excalidraw } from "@excalidraw/excalidraw";
-
+import { Excalidraw, WelcomeScreen } from "@excalidraw/excalidraw";
+import { useState } from "react";
+import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types.d.ts";
 const Whiteboard = () => {
+  const [excalidrawAPI, setExcalidrawAPI] =
+    useState<ExcalidrawImperativeAPI | null>(null);
+
+  const UIOptions = {
+    canvasActions: {
+      loadScene: false,
+    },
+    tools: { image: true },
+  };
+
   return (
     <div>
       <h1>Whiteboard</h1>
+      <button
+        type="button"
+        onClick={() => {
+          if (!excalidrawAPI) {
+            return;
+          }
+          const elements = excalidrawAPI.getSceneElements();
+          excalidrawAPI.scrollToContent(elements[0], {
+            fitToViewport: true,
+          });
+        }}
+      >
+        Fit to viewport, first element
+      </button>
+      <hr />
       <div style={{ height: "500px" }}>
-        <Excalidraw />
+        <Excalidraw
+          UIOptions={UIOptions}
+          excalidrawAPI={(api: ExcalidrawImperativeAPI) =>
+            setExcalidrawAPI(api)
+          }
+        >
+          <WelcomeScreen />
+        </Excalidraw>
       </div>
     </div>
   );
