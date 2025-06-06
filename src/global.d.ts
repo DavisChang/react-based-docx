139c763f23f7d6f908ea79ecb23812a079c685dc
Davis Chang <davischang1030@gmail.com>
2025-06-06 09:09:39 +0800

    Update global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,34 +1,33 @@
 import type { ElectronAPI } from "@electron-toolkit/preload";
 
-interface Window {
-  asyncBridge: {
-    [key: string]: (...args: any[]) => any;
-  };
-  chrome?: {
-    webview?: {
-      addEventListener: (event: string, listener: (event: any) => void) => void;
-      removeEventListener: (
-        event: string,
-        listener: (event: any) => void
-      ) => void;
-      postMessage: (message: any) => void;
+declare global {
+  interface Window {
+    asyncBridge: {
+      [key: string]: (...args: unknown[]) => unknown;
     };
-  };
-  dsBridge?: {
-    call: (method: string, args?: string) => any;
-    register: (method: string, handler: (data: string) => void) => void;
-    unregister: (method: string) => void;
-  };
+    chrome?: {
+      webview?: {
+        addEventListener: (event: string, listener: (event: MessageEvent) => void) => void;
+        removeEventListener: (
+          event: string,
+          listener: (event: MessageEvent) => void
+        ) => void;
+        postMessage: (message: unknown) => void;
+      };
+    };
+    dsBridge?: {
+      call: (method: string, args?: string) => unknown;
+      register: (method: string, handler: (data: string) => void) => void;
+      unregister: (method: string) => void;
+    };
+    electron: ElectronAPI;
+    parent: Window;
+    opener: Window | null;
+  }
 }
 
 declare namespace NodeJS {
   interface Process {
     type?: "browser" | "renderer" | "worker";
-  }
-}
-
-declare global {
-  interface Window {
-    electron: ElectronAPI
   }
 }
